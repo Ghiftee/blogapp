e3e579fddee6d3cec383ffb2f4a7b8456f27b0ea
Ghiftee <gigibetine@gmail.com>
2021-10-28 17:08:28 +0100

    Add image to user table

--- db/migrate/20211028151141_create_users.rb
+++ db/migrate/20211028151141_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       t.string :name, null: false
-      t.string :photo, null: false
+      t.string :photo, null: false, default: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fvectors%2Ftable-furniture-wooden-1300555%2F&psig=AOvVaw3theIKVGmB8Bhk-5pQMxEK&ust=1635523566809000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJCqjZi-7fMCFQAAAAAdAAAAABAI'
       t.text :bio, null: false
       t.timestamps
       t.integer :posts_counter, default: 0, null: false
