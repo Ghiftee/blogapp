bc2738cf97736596c9cde3abdf2daeab1ecffdd0
Ghiftee <gigibetine@gmail.com>
2021-11-02 09:25:31 +0100

    Create method that returns recent comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,13 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
+  has_many :likes
 
   def update_posts_counter(value)
     user.update('posts_counter', value)
+  end
+
+  def recent_posts(limit = 5)
+    comments.order(created_at: :desc).limit(limit)
   end
 end
