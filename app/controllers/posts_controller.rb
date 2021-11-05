7bda605995253feb20905352bd0ce5ebfc772d29
Ghiftee <gigibetine@gmail.com>
2021-11-05 21:13:35 +0100

    Create form structure

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,28 @@ class PostsController < ApplicationController
   def show
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
-    @comments = @post.comments.all
+    # @comments = @post.comments.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    new_post = Post.new(post_params)
+    current_user.posts << new_post
+
+    if new_post.save
+      redirect_to user_posts_path(new_post.user.id), notice: 'Post created!'
+    else
+      flash.now[:alert] = 'Failed to publish post!'
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
