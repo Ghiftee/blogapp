784933c0095f930c50ae682efb40eb57a26df7cc
Ghiftee <gigibetine@gmail.com>
2021-11-06 18:20:53 +0100

    Fix failing test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject { User.create(name: 'Cross', photo: 'CR', bio: 'My bio', posts_counter: 3) }
-
-  before { subject.save }
+  subject { User.create!(name: 'Cross', photo: 'CR', bio: 'My bio', posts_counter: 3) }
 
   describe 'validations' do
     it 'name should be present' do
@@ -17,11 +15,4 @@ RSpec.describe User, type: :model do
     end
   end
 
-  describe '#recent_posts' do
-    subject { User.first }
-
-    it 'should return the first 3 posts' do
-      expect(subject.recent_posts.length).to be(3)
-    end
-  end
 end
