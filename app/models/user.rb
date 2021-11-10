765d7b3aa7fed8ef4bace3ad4033f8a7f2be91f9
Ghiftee <gigibetine@gmail.com>
2021-11-10 14:23:10 +0100

    Complete devise setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :comments
   has_many :posts
   has_many :likes
 
-  validates :name, presence: { message: 'Name must not be blank' }
-  validates_numericality_of :posts_counter, only_integer: true, greater_than_or_equal: 0
+  # validates :name, presence: { message: 'Name must not be blank' }
+  # validates_numericality_of :posts_counter, only_integer: true, greater_than_or_equal: 0
 
   def recent_posts(limit = 3)
     posts.includes(:comments).order(created_at: :desc).limit(limit)
