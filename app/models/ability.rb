5ead22008c55f13e1087c5414f4689a70c500fe9
Ghiftee <gigibetine@gmail.com>
2021-11-10 20:30:11 +0100

    Fix rubocop errors

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,19 +1,15 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
+    return unless user.present?
 
-    if user.present?
-      if user.admin?
-        can :manage, :all
-      else
-        can [:read, :create], :all
-        can [:update, :destroy], Post, user: user
-        can :destroy, Comment, user: user
-      end
+    if user.admin?
+      can :manage, :all
+    else
+      can %i[read create], :all
+      can %i[update destroy], Post, user: user
+      can :destroy, Comment, user: user
     end
-    
   end
 end
