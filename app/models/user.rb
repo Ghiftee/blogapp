c391a3931ca32b542cfa787987630df3aea8b06e
Akasha <Akasha.Rojee@gmail.com>
2021-11-10 19:45:03 +0400

    Determine if user is admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,10 @@ class User < ApplicationRecord
   # validates :name, presence: { message: 'Name must not be blank' }
   # validates_numericality_of :posts_counter, only_integer: true, greater_than_or_equal: 0
 
+  def admin?
+    role == 'admin'
+  end
+
   def recent_posts(limit = 3)
     posts.includes(:comments).order(created_at: :desc).limit(limit)
   end
