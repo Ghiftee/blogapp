d5c763a6ebbaf7045f8e27241f8c854f98c6e96e
Ghiftee <gigibetine@gmail.com>
2021-11-10 19:51:11 +0100

    Update flash notice

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,11 +2,13 @@ class CommentsController < ApplicationController
   load_and_authorize_resource
 
   def create
-    post = Post.find(params[:id])
+    post = Post.find(params[:post_id])
     new_comment = Comment.create(user_id: current_user.id, post_id: post.id, text: comment_params[:text])
     if new_comment.save
-      redirect_to user_post_path(post.user.id, post.id), notice: 'Comment created!'
+      flash[:notice] = 'Comment created!'
+      redirect_to user_post_path(post.user.id, post.id)
     else
+      flash.now[:error] = 'Unable to create comment!'
       redirect_to user_post_path(post.user.id, post.id), alert: 'Failed to add comment!'
     end
   end
