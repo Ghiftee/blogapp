961feb2306539cdd2abc1b3638f8bea4d83a228c
Akasha <Akasha.Rojee@gmail.com>
2021-11-11 19:11:34 +0400

    Test error message on login without username and password

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -10,4 +10,10 @@ RSpec.feature 'Login page', type: :feature do
     expect(page.has_button?('Log in')).to be true
   end
 
+  scenario 'When I click the submit button without filling in the username and the password, I get a detailed error.' do
+    visit new_user_session_path
+    click_button 'Log in'
+    expect(page).to have_content 'Invalid Email or password'
+  end
+
 end
