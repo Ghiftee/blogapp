4b5a1273a34b6eb4ee01de84b620fbbf0e811b14
Akasha <Akasha.Rojee@gmail.com>
2021-11-13 01:30:32 +0400

    Add API endpoint and controller for listing all comments

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,3 +1,9 @@
 class Api::CommentsController < ApplicationController
+  load_and_authorize_resource
+
+  def index
+    @comments = Comment.where({ post_id: params[:post_id] }).order('created_at')
+    render json: { success: true, data: { comments: @comments } }
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     resources :posts, only: [:index] do
+      resources :comments, only: [:index]
     end
   end
 end
