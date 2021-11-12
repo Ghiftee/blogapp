c9d4cfb20748391dbfcd9ddfe137ebbf63671c95
Ghiftee <gigibetine@gmail.com>
2021-11-12 09:56:57 +0100

    Create like factory

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -2,12 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Like, type: :model do
   describe '#update_counter' do
-    let(:user) { User.create(name: 'Cross', photo: 'CR', bio: 'My bio', posts_counter: 3) }
-    let(:post) { user.posts.create(title: 'Post', text: 'Some text', comments_counter: 0, likes_counter: 1) }
+    subject { FactoryBot.build :like }
 
     it 'updates the likes counter' do
-      post.likes.create(user: user)
-      expect(post.likes_counter).to eq 1
+      expect(subject.post.likes_counter).to eq(1)
     end
   end
 end
