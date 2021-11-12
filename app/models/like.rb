881b4ac631a33e3fa866629cd55fd5510324cfb8
Ghiftee <gigibetine@gmail.com>
2021-11-12 10:05:39 +0100

    Update models spec

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,6 +3,6 @@ class Like < ApplicationRecord
   belongs_to :post
 
   def update_likes_counter(value)
-    post.update('likes_counter', value)
+    post.update(likes_counter: value)
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  describe '#update_counter' do
-    let(:user) { User.create(name: 'Cross', photo: 'CR', bio: 'My bio', posts_counter: 3) }
-    let(:post) { user.posts.create(title: 'Post', text: 'Some text', comments_counter: 1, likes_counter: 0) }
+  describe '#update_comments_counter' do
+    subject { FactoryBot.build :comment }
 
     it 'updates the post counter' do
-      post.comments.create(user: user, post: post, text: 'Some text')
-      expect(post.comments_counter).to eq 1
+      subject.update_comments_counter(4)
+      expect(subject.post.comments_counter).to be(4)
     end
   end
 end
