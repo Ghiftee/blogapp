d07ec53a632db1052d15b1e4e931531893d005f1
Akasha <Akasha.Rojee@gmail.com>
2021-11-13 02:41:43 +0400

    Correct authentication

--- db/schema.rb
+++ db/schema.rb
@@ -62,8 +62,10 @@ ActiveRecord::Schema.define(version: 2021_11_12_200326) do
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
     t.string "role"
+    t.string "jti", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
